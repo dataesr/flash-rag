ba1ed056658852de5785daebe7e30a8ffe6a5112
victor barbier <vicozer@gmail.com>
2026-05-19 15:43:13 +0200

    results by publications

--- src/query.py
+++ src/query.py
@@ -4,6 +4,42 @@ from src.chromadb import get_collection
 
 MAX_TIMESTAMP = datetime((datetime.now().year - 4), 1, 1).timestamp()  # 3 years ago + 1 year buffer
 
+
+def sources_to_publications(sources: list):
+    publications = {}
+    for source in sources:
+        publication = {
+            "file_name": source["metadata"].pop("file_name"),
+            "file_format": source["metadata"].pop("file_format"),
+            "doc_type": source["metadata"].pop("doc_type"),
+            "title": source["metadata"].pop("title"),
+            "created": source["metadata"].pop("created"),
+            "modified": source["metadata"].pop("modified"),
+            "publication_date": source["metadata"].pop("publication_date"),
+            "publication_epoch": source["metadata"].pop("publication_epoch"),
+            "keywords": source["metadata"].pop("keywords"),
+        }
+        publication_id = publication["file_name"]
+        if publication_id not in publications:
+            publications[publication_id] = publication
+            publications[publication_id]["sources"] = []
+
+        # Add source chunk
+        publications[publication_id]["sources"].append(source)
+        # Mean distance from chunks
+        publications[publication_id]["distance"] = sum(
+            [s["distance"] for s in publications[publication_id]["sources"]]
+        ) / len(publications[publication_id]["sources"])
+        # Sort sources by page and section
+        publications[publication_id]["sources"] = sorted(
+            publications[publication_id]["sources"],
+            key=lambda x: (x["metadata"]["page_index"], x["metadata"]["section_level"]),
+        )
+
+    # Sort publications by mean distance
+    return sorted(publications.values(), key=lambda x: x["distance"])
+
+
 def query(query_text: str, k: int = 5):
     # Get collection
     collection = get_collection()
@@ -31,7 +67,7 @@ def query(query_text: str, k: int = 5):
 
     answer = "AI answer is not implemented yet..."
 
-    return answer, sources
+    return answer, sources, sources_to_publications(sources)
 
 
 def query_cli():
